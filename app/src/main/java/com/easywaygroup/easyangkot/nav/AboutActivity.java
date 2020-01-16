5784fa2835889708803fd5c16541d08b33f93398
bagusth <bagusth>
2020-01-16 13:05:45 +0700

    add about.xml still developing by Bagus

--- app/src/main/java/com/easywaygroup/easyangkot/nav/AboutActivity.java
+++ app/src/main/java/com/easywaygroup/easyangkot/nav/AboutActivity.java
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.acer.angkotroutes.R;
 
-public class FavoriteActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorite);
+        setContentView(R.layout.activity_about);
     }
 }
