226786ef012da5d358538ccec4931645fd93387d
bagusth <bagusth>
2020-01-23 11:09:14 +0700

    add splash screen

---
+++ app/src/main/java/com/easywaygroup/easyangkot/nav/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.easywaygroup.easyangkot.nav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.acer.angkotroutes.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Handler handler = new Handler();
+        handler.postDelayed(runnable,3000);
+    }
+}
